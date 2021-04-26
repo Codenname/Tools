2e1da85732510022452c0c5dd92cb23a7b255f5f
Codenname <80440658+Codenname@users.noreply.github.com>
2021-04-25 22:43:56 -0400

    Add files via upload

---
+++ pwncat/commands/alias.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+import pwncat
+from pwncat.commands.base import CommandDefinition, Complete, Parameter
+from pwncat.util import console
+from colorama import Fore
+
+
+class Command(CommandDefinition):
+    """ Alias an existing command with a new name. Specifying no alias or command
+    will list all aliases. Specifying an alias with no command will remove the 
+    alias if it exists. """
+
+    def get_command_names(self):
+        return [c.PROG for c in pwncat.victim.command_parser.commands]
+
+    PROG = "alias"
+    ARGS = {
+        "alias": Parameter(Complete.NONE, help="name for the new alias", nargs="?"),
+        "command": Parameter(
+            Complete.CHOICES,
+            metavar="COMMAND",
+            choices=get_command_names,
+            help="the command the new alias will use",
+            nargs="?",
+        ),
+    }
+    LOCAL = True
+
+    def run(self, args):
+        if args.alias is None:
+            for name, command in pwncat.victim.command_parser.aliases.items():
+                console.print(
+                    f" [cyan]{name}[/cyan] \u2192 [yellow]{command.PROG}[/yellow]"
+                )
+        elif args.command is not None:
+            # This is safe because of "choices" in the argparser
+            pwncat.victim.command_parser.aliases[args.alias] = [
+                c
+                for c in pwncat.victim.command_parser.commands
+                if c.PROG == args.command
+            ][0]
+        else:
+            del pwncat.victim.command_parser.aliases[args.alias]
---
+++ pwncat/commands/exit.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import pwncat
+from pwncat.util import console
+from pwncat.commands.base import CommandDefinition, Complete, Parameter
+
+
+class Command(CommandDefinition):
+    """
+    Exit pwncat. You must provide the "--yes" parameter.
+    This prevents accidental closing of your remote session.
+    """
+
+    PROG = "exit"
+    ARGS = {
+        "--yes,-y": Parameter(
+            Complete.NONE,
+            action="store_true",
+            help="Confirm you would like to close pwncat",
+        )
+    }
+    LOCAL = True
+
+    def run(self, args):
+
+        # Ensure we confirmed we want to exit
+        if not args.yes:
+            console.log("[red]error[/red]: exit not confirmed (use '--yes')")
+            return
+
+        # Get outa here!
+        raise pwncat.util.CommandSystemExit
